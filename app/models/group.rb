f367a186c8b76b85f12ebfa8566d65a489c03b6e
Christian Ani <iamchristianani@gmail.com>
2023-04-25 17:10:06 +0100

    Update validations and relationships for groups and expenses table

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,11 @@
 class Group < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_and_belongs_to_many :expenses
+  has_many :expenses_groups, class_name: 'ExpenseGroup', foreign_key: 'group_id', dependent: :destroy
+
+  validates :name, presence: true
+  validates :icon, presence: true
+
+  def total_price
+    expenses_groups.sum { |expgro| expgro.expense.amount }
+  end
 end
