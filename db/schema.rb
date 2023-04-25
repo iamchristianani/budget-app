48891cbf84fcb1fa2ec1a97634de71e790c64e21
Christian Ani <iamchristianani@gmail.com>
2023-04-25 17:11:12 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_24_153117) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_25_142134) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "expense_groups", force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.bigint "expense_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["expense_id"], name: "index_expense_groups_on_expense_id"
+    t.index ["group_id"], name: "index_expense_groups_on_group_id"
+  end
+
   create_table "expenses", force: :cascade do |t|
     t.string "name"
-    t.decimal "amount"
+    t.integer "amount"
     t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_expenses_on_author_id"
-  end
-
-  create_table "expenses_groups", id: false, force: :cascade do |t|
-    t.bigint "group_id", null: false
-    t.bigint "expense_id", null: false
-    t.index ["expense_id", "group_id"], name: "index_expenses_groups_on_expense_id_and_group_id"
-    t.index ["group_id", "expense_id"], name: "index_expenses_groups_on_group_id_and_expense_id"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -56,6 +58,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_24_153117) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "expense_groups", "expenses"
+  add_foreign_key "expense_groups", "groups"
   add_foreign_key "expenses", "users", column: "author_id"
   add_foreign_key "groups", "users", column: "author_id"
 end
