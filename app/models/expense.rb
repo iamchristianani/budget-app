a02ec4d72e9e85a3f2aca274d7b7e43e2ba78f31
Christian Ani <iamchristianani@gmail.com>
2023-04-27 11:32:41 +0100

    Update application models

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,7 @@
 class Expense < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :expenses_groups, class_name: 'ExpenseGroup', foreign_key: 'expense_id', dependent: :destroy
+  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
+  has_many :expense_groups, class_name: 'ExpenseGroup', foreign_key: 'expense_id'
+  has_many :groups, through: :expense_groups
 
   validates :name, presence: true
   validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
