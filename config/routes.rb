91340f1d0837d46e673caf24e6f4ce7f1f4a615f
Christian Ani <iamchristianani@gmail.com>
2023-04-27 11:36:02 +0100

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,25 @@
-Rails.application.routes.draw do  devise_for :users
+Rails.application.routes.draw do  
+  get '/splash', to: 'splash#index'
+  
+  devise_for :users, sign_out_via: [:get, :post]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root "home#index"
+  devise_scope :user do
+    authenticated :user do
+      root 'groups#index', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root "splash#index", as: :unauthenticated_root
+    end
+  end
 
-  resources :groups, only: [:index, :new, :create]
-  resources :expenses, only: [:index, :new, :create]
+  resources :groups, only: [:index, :new, :create] do
+    resources :expenses, only: [:index]
+  end
+  resources :expenses, only: [:new, :create]
 end
